24a5f9353ace06c76918d4a478c1ba470ded4f4f
pione30 <g.pionelish30@gmail.com>
2021-03-08 01:24:27 +0900

    add .gce <instance-name> (start|stop) command

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,79 @@ discordClient.on("ready", () => {
   console.log("I am ready!");
 });
 
-discordClient.on("message", (message) => {
-  if (message.content === "ping") {
-    message.channel.send("pong");
+type operationType = "start" | "stop";
+
+const powerInstance = async (
+  message: Discord.Message,
+  instanceName: string,
+  operationName: operationType,
+  zoneName?: string
+): Promise<void> => {
+  const zone = googleCompute.zone(zoneName ?? process.env.DEFAULT_COMPUTE_ZONE);
+  const vm = zone.vm(instanceName);
+
+  switch (operationName) {
+    case "start": {
+      message.channel.send(`${instanceName} starting...`);
+
+      try {
+        const [operation] = await vm.start();
+        await operation.promise();
+      } catch (error) {
+        message.channel.send(`Failed to start ${instanceName}: ${error}`);
+        return;
+      }
+
+      message.channel.send(`${instanceName} started!`);
+
+      try {
+        const vmData = await vm.get();
+        message.channel.send(
+          `IP address: ${vmData[0].metadata.networkInterfaces[0].accessConfigs[0].natIP}`
+        );
+      } catch (error) {
+        message.channel.send(`Failed to get ${instanceName}: ${error}`);
+        return;
+      }
+
+      break;
+    }
+    case "stop": {
+      message.channel.send(`${instanceName} stopping...`);
+
+      try {
+        const [operation] = await vm.stop();
+        await operation.promise();
+      } catch (error) {
+        message.channel.send(`Failed to stop ${instanceName}: ${error}`);
+        return;
+      }
+
+      message.channel.send(`${instanceName} stopped!`);
+
+      break;
+    }
+    default: {
+      const strangeOperationName: never = operationName;
+      throw new Error(`${strangeOperationName} is strange operation name!`);
+    }
+  }
+};
+
+discordClient.on("message", async (message) => {
+  const commands = message.content.split(" ");
+  if (commands[0] === ".gce") {
+    const [_dotGce, instanceName, operationName, zoneName] = commands;
+
+    if (
+      commands.length < 3 ||
+      (operationName !== "start" && operationName !== "stop")
+    ) {
+      message.channel.send("Usage: `.gce <instance-name> (start|stop)`");
+      return;
+    }
+
+    powerInstance(message, instanceName, operationName, zoneName);
   }
 });
 
